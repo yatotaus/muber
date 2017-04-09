c7cfa1e440cd56d5a11ed13e32b34e3cf724a869
Marcos Matias <marcosmatias_iriarte@hotmail.com>
2017-04-09 15:29:11 -0300

    Persistiendo tabla usuarios

---
+++ src/main/java/test/Prueba.java
@@ -0,0 +1,42 @@
+package test;
+
+import java.io.File;
+import java.sql.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import clases.Passenger;
+
+public class Prueba {
+	
+	private static SessionFactory sessionFactory = buildSessionFactory();
+	
+	private static SessionFactory buildSessionFactory(){
+		try {
+			return new Configuration().configure(new File("src/main/resources/hibernate/hibernate.cfg.xml")).buildSessionFactory();
+		}
+		catch(Throwable ex){
+			
+			System.err.println("Error de hibernate. No funciona " + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+
+	public static void main(String[] args) {
+		Passenger u = new Passenger();
+		u.setCredits(1500);
+		u.setUser_date(new Date(10, 3, 2017));
+		u.setUser_name("German");
+		u.setUser_password("contraseña");
+		
+		Session s = sessionFactory.getCurrentSession();
+		s.beginTransaction();
+		s.save(u);
+		s.getTransaction().commit();
+
+	}
+
+}
