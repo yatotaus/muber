ef446cdad93c391e56445675e72c7c0f785dfae9
Marcos Matias <marcosmatias_iriarte@hotmail.com>
2017-04-09 10:13:36 -0300

    Prueba git

---
+++ src/main/java/test/PruebaPersistencia.java
@@ -0,0 +1,17 @@
+package test;
+
+import junit.framework.TestCase;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+/**
+ * @author marcos
+ *
+ */
+public class PruebaPersistencia extends TestCase {
+
+}
