8990869cf37a19efbc3e39eaba7a24f5faa3a77d
Marcos Matias <marcosmatias_iriarte@hotmail.com>
2017-04-10 12:18:17 -0300

    Persistiendo score

--- src/main/java/clases/Score.java
+++ src/main/java/clases/Score.java
@@ -4,80 +4,53 @@
 package clases;
 
 /**
- * @author yato
+ * @author marcos
  *
  */
 public class Score {
+
+	/**
+	 * 
+	 */
 	
 	private Long score_id;
 	private String score_comment;
 	private Integer score_number;
-	
-	
-	/**
-	 * 
-	 */
+
 	public Score() {
 		// TODO Auto-generated constructor stub
 	}
-
-
-	/**
-	 * 
-	 */
-	public Score(String comment, Integer score) {
+	
+	public Score(String score_comment, Integer score_number) {
 		super();
-		this.setComment(comment);
-		this.setScore(score);
+		this.score_comment = score_comment;
+		this.score_number = score_number;
 	}
 
 
-	/**
-	 * @return the score_id
-	 */
 	public Long getScore_id() {
 		return score_id;
 	}
 
-
-	/**
-	 * @param score_id the score_id to set
-	 */
 	public void setScore_id(Long score_id) {
 		this.score_id = score_id;
 	}
 
-
-	/**
-	 * @return the comment
-	 */
-	public String getComment() {
+	public String getScore_comment() {
 		return score_comment;
 	}
 
-
-	/**
-	 * @param comment the comment to set
-	 */
-	public void setComment(String comment) {
-		this.score_comment = comment;
+	public void setScore_comment(String score_comment) {
+		this.score_comment = score_comment;
 	}
 
-
-	/**
-	 * @return the score
-	 */
-	public Integer getScore() {
+	public Integer getScore_number() {
 		return score_number;
 	}
 
-
-	/**
-	 * @param score the score to set
-	 */
-	public void setScore(Integer score) {
-		this.score_number = score;
+	public void setScore_number(Integer score_number) {
+		this.score_number = score_number;
 	}
 	
-	
+
 }
--- src/main/java/test/Prueba.java
+++ src/main/java/test/Prueba.java
@@ -9,6 +9,7 @@ import org.hibernate.cfg.Configuration;
 
 import clases.Driver;
 import clases.Passenger;
+import clases.Score;
 import clases.Trip;
 
 public class Prueba {
@@ -36,11 +37,15 @@ public class Prueba {
 		u.setUser_password("contraseña");
 		
 		Trip t = new Trip(900,4,new Date(22, 04, 2017),"La Plata", "Tres Arroyos");
+		Score score = new Score();
+		score.setScore_comment("Muy buen conductor");
+		score.setScore_number(4);
+
 
 		
 		Session s = sessionFactory.getCurrentSession();
 		s.beginTransaction();
-		s.save(u);
+		s.save(score);
 		s.getTransaction().commit();
 
 	}
